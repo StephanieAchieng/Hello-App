de3aed143682575edc0c4dc3967a8f595d027607
StephanieAchieng <stephachie02@gmail.com>
2020-08-22 03:41:09 +0300

    initial commit

--- app/src/main/java/com/example/hello/CoursesActivity.kt
+++ app/src/main/java/com/example/hello/CoursesActivity.kt
@@ -2,32 +2,51 @@ package com.example.hello
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.telecom.Call
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.row_courses_item.*
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
-data class Courses(val course_id: Int, val course_name: String, val course_code: Int, val instructor:String, val description:String)
+//data class Courses(val course_id: Int, val course_name: String, val course_code: Int, val instructor:String, val description:String)
 
 class CoursesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_courses)
 
-        rvCourses.LayoutManager = LinearLayoutManager(baseContext)
-        val coursesRecyclerViewAdapter = CoursesRecyclerViewAdapter(coursesList = listOf(
+        fetchCourses()
 
-            Courses(20,"Design",57,"Jane","Creativity"),
-            Courses(90,"Kotlin",49,"Arie","Android Development"),
-            Courses(65,"Python",35,"James","Databases"),
-            Courses(84,"Html/Css",82,"Jeff","Web Development"),
-            Courses(27,"Javascript",28,"Lean","React"),
-            Courses(70,"Navigating",58,"Grey","Navigation"),
-            Courses(56,"C++",40,"Roy","Electricals"),
-            Courses(39,"Product Design",76,"Barre","Drafting"),
-            Courses(69,"Entreprenuership",25,"Kelly Murungi","Finance"),
-            Courses(50,"Hardware Electronics",33,"Barre Yasin","Arduino")
-
-
-        ))
-        rvCourses.adapter=CoursesRecyclerViewAdapter
     }
+     fun fetchCourses() {
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(baseContext)
+        val accessToken = sharedPreferences.getString("ACCESS_TOKEN_KEY", "")
+
+        val apiClient = ApiClient.buildService(ApiInterface::class.java)
+        val coursesCall = apiClient.getCourses("Bearer " + accessToken)
+        coursesCall.enqueue(object : Callback<CoursesResponse> {
+            override fun onFailure(call: Call<CoursesResponse>, t: Throwable) {
+                Toast.makeText(baseContext, t.message, Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(
+                call: Call<CoursesResponse>,
+                response: Response<CoursesResponse>
+            ) {
+                if (response.isSuccessful) {
+                    var courseList = response.body()?.courses as List<Course>
+                    var coursesAdapter = CoursesRecyclerViewAdapter(courseList)
+                    rvCourses.layoutManager = LinearLayoutManager(baseContext)
+                    rvCourses.adapter = coursesAdapter
+                } else {
+                    Toast.makeText(baseContext, response.errorBody().toString(), Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+        })
+    }
+
+
 }
--- app/src/main/res/layout/ApiInterface.kt
+++ app/src/main/res/layout/ApiInterface.kt
@@ -1,5 +1,6 @@
 package com.example.hello
 
+import com.example.Hello.RegistrationResponse
 import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.Body
@@ -10,6 +11,7 @@ interface ApiInterface {
     @POST("register")
 
     fun registerStudent(@Body requestBody: RequestBody): Call<RegistrationResponse>
+    abstract fun getCourses(s: String): Any
 }
 
 
